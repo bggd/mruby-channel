9acdba5c3d54e916a4e00152bc54cf603815152e
Nick Bernardi <no.one.wins@gmail.com>
2017-07-07 14:30:49 -0700

    wrap cases that declare variables in {}. Fixes "jump to case label" error.

--- chan_src/variant.cpp
+++ chan_src/variant.cpp
@@ -75,18 +75,20 @@ Variant Variant::from_mruby(mrb_state* mrb, mrb_value v)
     case MRB_TT_FIXNUM: return Variant(mrb_fixnum(v));
     case MRB_TT_FLOAT: return Variant(mrb_float(v));
     case MRB_TT_STRING: return Variant(Variant::Type::STRING, RSTRING_PTR(v), RSTRING_LEN(v));
-    case MRB_TT_SYMBOL:
+    case MRB_TT_SYMBOL: {
       mrb_int len;
       str = mrb_sym2name_len(mrb, mrb_symbol(v), &len);
       return Variant(Variant::Type::SYMBOL, (char*)str, len);
-    case MRB_TT_ARRAY:
+    }
+    case MRB_TT_ARRAY: {
       ary = new std::vector<Variant>;
       ary->reserve(RARRAY_LEN(v));
       for (mrb_int i=0; i < RARRAY_LEN(v); ++i) {
         ary->push_back(Variant::from_mruby(mrb, RARRAY_PTR(v)[i]));
       }
       return Variant(ary);
-    case MRB_TT_HASH:
+    }
+    case MRB_TT_HASH: {
       mrb_value keys = mrb_hash_keys(mrb, v);
       mrb_value values = mrb_hash_values(mrb, v);
       tbl = new std::vector<std::pair<Variant, Variant>>;
@@ -97,6 +99,7 @@ Variant Variant::from_mruby(mrb_state* mrb, mrb_value v)
         tbl->push_back(std::make_pair(Variant::from_mruby(mrb, k), Variant::from_mruby(mrb, val)));
       }
       return Variant(tbl);
+    }
     default: break;
   }
 
@@ -110,23 +113,26 @@ mrb_value Variant::to_mruby(mrb_state* mrb)
     case Variant::Type::BOOL: return mrb_bool_value(this->data.boolean);
     case Variant::Type::FIXNUM: return mrb_fixnum_value(this->data.integer);
     case Variant::Type::FLOAT: return mrb_float_value(mrb, this->data.number);
-    case Variant::Type::STRING:
+    case Variant::Type::STRING: {
       mrb_value str = mrb_str_buf_new(mrb, this->data.len);
       return mrb_str_cat(mrb, str, this->data.str, this->data.len);
+    }
     case Variant::Type::SYMBOL:
       return mrb_symbol_value(mrb_intern(mrb, this->data.str, this->data.len));
-    case Variant::Type::ARRAY:
+    case Variant::Type::ARRAY: {
       mrb_value ary = mrb_ary_new_capa(mrb, this->data.ary->size());
       for (auto it=this->data.ary->begin(); it != this->data.ary->end(); ++it) {
         mrb_ary_push(mrb, ary, it->to_mruby(mrb));
       }
       return ary;
-    case Variant::Type::HASH:
+    }
+    case Variant::Type::HASH: {
       mrb_value tbl = mrb_hash_new_capa(mrb, this->data.tbl->size());
       for (auto it=this->data.tbl->begin(); it != this->data.tbl->end(); ++it) {
         mrb_hash_set(mrb, tbl, it->first.to_mruby(mrb), it->second.to_mruby(mrb));
       }
       return tbl;
+    }
     default: break;
   }
   return mrb_nil_value();
